8a1ee2cc39aa574aeefcbf310d6eaa7c99e4a914
Connor Kormos <connorkormos97@gmail.com>
2023-04-04 10:36:34 -0700

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    i = 0
+    my_list = []
+    my_range = [*range(length)]
+    while len(my_list) < len(my_range):
+        if (i == 0):
+            my_list.append(i)
+            i += 1
+        elif (i == 1):
+            my_list.append(i)          
+            i += my_list[-2]
+        else:
+            i = my_list[-1] + my_list[-2]
+            my_list.append(i)
+    print(my_list)
+
+print_fibonacci(9)
